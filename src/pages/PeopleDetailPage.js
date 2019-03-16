c168882ca83697376ee497ec88bea6335b273e46
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-03-16 10:46:58 -0300

    Dados na página PeopleDetailPage

--- src/pages/PeopleDetailPage.js
+++ src/pages/PeopleDetailPage.js
@@ -1,12 +1,50 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image } from 'react-native'
 
 export default class PeopleDetailPage extends React.Component {
     render() {
-        return(
-            <View>
-                <Text>Eu sou a people detail page!</Text>
+        const { people } = this.props.navigation.state.params
+
+        return (
+            <View style={styles.container}>
+                <Image style={styles.avatar} source={{ uri: people.picture.large }} />
+                <View style={styles.detailContainer}>
+                    <View style={styles.line}>
+                        <Text style={styles.cellLabel}>E-mail: </Text>
+                        <Text style={styles.cellContent}>{people.email}</Text>
+                    </View>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 15
+    },
+    avatar: {
+        aspectRatio: 1
+    },
+    detailContainer: {
+        backgroundColor: '#e2f9ff',
+        marginTop: 20,
+        elevation: 1
+    },
+    line: {
+        flexDirection: 'row',
+        paddingTop: 3,
+        paddingBottom: 3,
+        borderWidth: 1,
+        borderColor: '#c5c5c5'
+    },
+    cellLabel: {
+        fontSize: 18,
+        paddingLeft: 5,
+        fontWeight: 'bold'
+    },
+    cellContent: {
+        fontSize: 18,
+        paddingLeft: 5
+    }
+})
