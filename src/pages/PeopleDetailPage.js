7be62d40e193a35465ba5e3ab32fc4876ab50018
Luis Paulo Ruda <luispauloruda@gmail.com>
2019-03-18 21:42:09 -0300

    Separação do componente Line

--- src/pages/PeopleDetailPage.js
+++ src/pages/PeopleDetailPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { StyleSheet, View, Text, Image } from 'react-native'
 
+import Line from '../components/Line'
+
 export default class PeopleDetailPage extends React.Component {
     render() {
         const { people } = this.props.navigation.state.params
@@ -9,10 +11,12 @@ export default class PeopleDetailPage extends React.Component {
             <View style={styles.container}>
                 <Image style={styles.avatar} source={{ uri: people.picture.large }} />
                 <View style={styles.detailContainer}>
-                    <View style={styles.line}>
-                        <Text style={styles.cellLabel}>E-mail: </Text>
-                        <Text style={styles.cellContent}>{people.email}</Text>
-                    </View>
+                    <Line label="Email:" content={people.email} />
+                    <Line label="Cidade:" content={people.location.city} />
+                    <Line label="Estado:" content={people.location.state} />
+                    <Line label="Tel:" content={people.phone} />
+                    <Line label="Cel:" content={people.cell} />
+                    <Line label="Nat:" content={people.nat} />
                 </View>
             </View>
         )
@@ -30,21 +34,5 @@ const styles = StyleSheet.create({
         backgroundColor: '#e2f9ff',
         marginTop: 20,
         elevation: 1
-    },
-    line: {
-        flexDirection: 'row',
-        paddingTop: 3,
-        paddingBottom: 3,
-        borderWidth: 1,
-        borderColor: '#c5c5c5'
-    },
-    cellLabel: {
-        fontSize: 18,
-        paddingLeft: 5,
-        fontWeight: 'bold'
-    },
-    cellContent: {
-        fontSize: 18,
-        paddingLeft: 5
     }
 })
